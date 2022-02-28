cf254754ac6fa90897f2be74bdcea4d1ce12316a
rakibul-wdp <rakibul.wdp@gmail.com>
2022-02-28 23:50:21 +0600

    get data form api

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-console.log("ki obosta");
+// get value form search box
+const searchPhone = () => {
+  const searchBox = document.getElementById('search-box');
+  const searchText = searchBox.value;
+  // clear search box
+  searchBox.value = '';
+  // get data
+  const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => console.log(data.data));
+};
